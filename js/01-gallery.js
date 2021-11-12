1876e7c8b963d70fa82eedaebbd1ea66d8b5a5bc
Pavel-Baturin <pavel.baturin.92@gmail.com>
2021-11-12 09:25:18 +0200

    Дщбавляет решение задач №1 и №2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const galleryImages = document.querySelector('.gallery');
+const murkupGallery = createGalleryImages(galleryItems);
+galleryImages.insertAdjacentHTML('beforeend', murkupGallery);
+galleryImages.addEventListener('click', onImageClick)
+
+
+function createGalleryImages(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`;
+    }).join("");
+}
+
+function onImageClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+    
+    const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src="${event.target.dataset.source}">
+    </div>
+    `)
+    instance.show()
+    const modal = document.querySelector('.modal');
+    modal.addEventListener('click', () => instance.close())
+    
+}
+    
